c5cb27104633c1fa398806c833c97d39d6053b1b
Robert <robertionut.rr@gmail.com>
2021-10-07 23:09:16 +0300

    second commit

--- session3/ex4.py
+++ session3/ex4.py
@@ -11,7 +11,9 @@ t2 = (3, 4, 5)
 # Vom crea variabila l1 iar ca si valoare, va avea o lista compusa din
 # concatenarea celor 2 tupluri. Primele elemente vor fi cele din l1.
 # Vom converti cele 2 tupluri in liste, inainte sa le concatenam
-l1 = list(t1) + list(t2)
+x=list(t1)
+x.append(2)
+l1 = x + list(t2)
 
 # Afisam lista
 print(l1)
